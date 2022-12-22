e88699c64a0b3851b969b9f9e732f35c6dd7af06
MhSolyman <solymanbijoye.com>
2022-12-22 18:26:29 +0600

    add footer

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -4,7 +4,7 @@ import Course from '../Pages/Course/Course';
 import Home from '../Pages/Home/Home';
 import Header from '../Pages/Shared/Header/Header';
 import Right from '../Pages/Shared/Right/Right';
-
+import './mar.css'
 
 
 const Main = () => {
@@ -20,10 +20,11 @@ const Main = () => {
                             <Course></Course>
 
                         </Outlet>
+                       
 
                     </div>
 
-                    <div className="col-sm-4">
+                    <div className="col-sm-4  ">
                         <Right>
 
                         </Right>
@@ -32,7 +33,10 @@ const Main = () => {
 
 
             </div>
-
+            <div className='ma'><div className='bg'>Phone: 01324129983 <br />
+            <div> Email: solymanbijoy@gmail.com</div>
+            <div>Facebook: https://www.facebook.com/Mhsolyman1/</div>
+            </div></div>
         </div>
     );
 };
